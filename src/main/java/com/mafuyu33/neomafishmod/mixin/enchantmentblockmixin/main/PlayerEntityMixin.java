dbcae0fc86885f95045048aee8ab1a5ccb5271ab
mafuyu33 <jzh654354772@gmail.com>
2024-12-15 19:10:44 +0000

    优化刷子，下调了遍历的范围，防止卡顿

--- src/main/java/com/mafuyu33/neomafishmod/mixin/enchantmentblockmixin/main/PlayerEntityMixin.java
+++ src/main/java/com/mafuyu33/neomafishmod/mixin/enchantmentblockmixin/main/PlayerEntityMixin.java
@@ -10,79 +10,102 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Mixin(Player.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
+	@Unique
+	private int neomafishmod$tickCounter = 0;
+	@Unique
+	private final Set<BlockPos> neomafishmod$enchantedBlocks = new HashSet<>();
+
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, Level level) {
 		super(entityType, level);
 	}
 
 	@Inject(at = @At("HEAD"), method = "tick")
 	private void init(CallbackInfo info) {
-		if(this.level().isClientSide && this.isHolding(Items.BRUSH)){
-			// 获取玩家所在位置
-			BlockPos playerPos = this.blockPosition();
+		if (this.level().isClientSide && this.isHolding(Items.BRUSH)) {
+			neomafishmod$tickCounter++;
+			if (neomafishmod$tickCounter % 20 == 0) {
+				// 每20个tick更新一次附魔方块位置
+				updateEnchantedBlocks();
+			}
 
-			// 遍历玩家周围 10 格范围内的方块
-			for (int x = -10; x <= 10; x++) {
-				for (int y = -10; y <= 10; y++) {
-					for (int z = -10; z <= 10; z++) {
-						BlockPos blockPos = playerPos.offset(x, y, z);
+			// 每个tick在附魔方块位置生成粒子
+			for (BlockPos blockPos : neomafishmod$enchantedBlocks) {
+				addParticles(blockPos);
+			}
+		}
+	}
 
-						// 检查方块是否有附魔
-						if (!Objects.equals(BlockEnchantmentStorage.getEnchantmentsAtPosition(blockPos), new ListTag())) {
-							// 在方块上创建粒子效果
-							// 在方块顶部创建粒子效果
-							this.level().addParticle(ParticleTypes.COMPOSTER,
-									blockPos.getX() + 0.5,
-									blockPos.getY() + 1.1,
-									blockPos.getZ() + 0.5,
-									0.0, 0.0, 0.0);
+	private void updateEnchantedBlocks() {
+		neomafishmod$enchantedBlocks.clear();
+		BlockPos playerPos = this.blockPosition();
 
-							// 在方块底部创建粒子效果
-							this.level().addParticle(ParticleTypes.COMPOSTER,
-									blockPos.getX() + 0.5,
-									blockPos.getY()-0.1,
-									blockPos.getZ() + 0.5,
-									0.0, 0.0, 0.0);
+		// 遍历玩家周围 8 格范围内的方块
+		for (int x = -8; x <= 8; x++) {
+			for (int y = -8; y <= 8; y++) {
+				for (int z = -8; z <= 8; z++) {
+					BlockPos blockPos = playerPos.offset(x, y, z);
 
-							// 在方块北侧创建粒子效果
-							this.level().addParticle(ParticleTypes.COMPOSTER,
-									blockPos.getX() + 0.5,
-									blockPos.getY() + 0.5,
-									blockPos.getZ()-0.1, // 在北侧中心位置
-									0.0, 0.0, 0.0);
-
-							// 在方块南侧创建粒子效果
-							this.level().addParticle(ParticleTypes.COMPOSTER,
-									blockPos.getX() + 0.5,
-									blockPos.getY() + 0.5,
-									blockPos.getZ()+1.1, // 在南侧中心位置
-									0.0, 0.0, 0.0);
-
-							// 在方块西侧创建粒子效果
-							this.level().addParticle(ParticleTypes.COMPOSTER,
-									blockPos.getX()-0.1, // 在西侧中心位置
-									blockPos.getY() + 0.5,
-									blockPos.getZ() + 0.5,
-									0.0, 0.0, 0.0);
-
-							// 在方块东侧创建粒子效果
-							this.level().addParticle(ParticleTypes.COMPOSTER,
-									blockPos.getX() + 1.1, // 在东侧中心位置
-									blockPos.getY() + 0.5,
-									blockPos.getZ() + 0.5,
-									0.0, 0.0, 0.0);
-
-						}
+					// 检查方块是否有附魔
+					if (!Objects.equals(BlockEnchantmentStorage.getEnchantmentsAtPosition(blockPos), new ListTag())) {
+						neomafishmod$enchantedBlocks.add(blockPos);
 					}
 				}
 			}
 		}
+	}
+
+	private void addParticles(BlockPos blockPos) {
+		// 在方块顶部创建粒子效果
+		this.level().addParticle(ParticleTypes.COMPOSTER,
+				blockPos.getX() + 0.5,
+				blockPos.getY() + 1.1,
+				blockPos.getZ() + 0.5,
+				0.0, 0.0, 0.0);
+
+		// 在方块底部创建粒子效果
+		this.level().addParticle(ParticleTypes.COMPOSTER,
+				blockPos.getX() + 0.5,
+				blockPos.getY() - 0.1,
+				blockPos.getZ() + 0.5,
+				0.0, 0.0, 0.0);
+
+		// 在方块北侧创建粒子效果
+		this.level().addParticle(ParticleTypes.COMPOSTER,
+				blockPos.getX() + 0.5,
+				blockPos.getY() + 0.5,
+				blockPos.getZ() - 0.1,
+				0.0, 0.0, 0.0);
+
+		// 在方块南侧创建粒子效果
+		this.level().addParticle(ParticleTypes.COMPOSTER,
+				blockPos.getX() + 0.5,
+				blockPos.getY() + 0.5,
+				blockPos.getZ() + 1.1,
+				0.0, 0.0, 0.0);
+
+		// 在方块西侧创建粒子效果
+		this.level().addParticle(ParticleTypes.COMPOSTER,
+				blockPos.getX() - 0.1,
+				blockPos.getY() + 0.5,
+				blockPos.getZ() + 0.5,
+				0.0, 0.0, 0.0);
+
+		// 在方块东侧创建粒子效果
+		this.level().addParticle(ParticleTypes.COMPOSTER,
+				blockPos.getX() + 1.1,
+				blockPos.getY() + 0.5,
+				blockPos.getZ() + 0.5,
+				0.0, 0.0, 0.0);
 	}
 }
